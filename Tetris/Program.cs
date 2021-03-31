4aded33d2f3903f2859fc60707c8c0a13dad06a3
Flip <222reviews@gmail.com>
2021-03-31 21:50:09 +0300

    Testing GitBash

---
+++ Tetris/Program.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Diagnostics;
+using System.Media;
+
+namespace Tetris
+{
+     class Program
+    {    
+        
+            public static int[,] area = new int[23, 10];
+            public static int[,] positionOnGrid = new int[23, 10];
+            public static string block = "■";
+            public static bool letFall = false;
+            public static Stopwatch timer = new Stopwatch();
+            public static Stopwatch timerC = new Stopwatch();
+            public static Stopwatch timerE = new Stopwatch();
+            public static int time, speed = 300;
+            static Tetrimino tetrimino;
+            static Tetrimino nextTetrimino;
+            public static ConsoleKeyInfo key;
+            public static bool keyPulse = false;
+            public static int lineScore = 0, points = 0, level = 1;
+        
+         static void Main()
+        {
+            
+            GameArea();
+            Console.SetCursorPosition(4, 5);
+            Console.WriteLine("Press Any Key");
+            Console.ReadKey(true);
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = Environment.CurrentDirectory + "\\Tetris.wav";
+            sp.PlayLooping();
+            timer.Start();
+            timerC.Start();
+            _ = timer.ElapsedMilliseconds;
+            Console.SetCursorPosition(25, 0);
+            Console.WriteLine("Level: " + level);
+            Console.SetCursorPosition(25, 1);
+            Console.WriteLine("Points: " + points);
+            Console.SetCursorPosition(25, 2);
+            Console.WriteLine("Score: " + lineScore);
+            nextTetrimino = new Tetrimino();
+            tetrimino = nextTetrimino;
+            tetrimino.Appeareance();
+            nextTetrimino = new Tetrimino();
+
+            Update();
+            sp.Stop();
+            sp.SoundLocation = Environment.CurrentDirectory + "\\Retro-game-over-sound-effect.wav";
+            sp.Play();
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("Game Over \n Start New Game? (Y/N)");
+            string teclaPulsada = Console.ReadLine();
+
+            if (teclaPulsada.ToUpper() == "Y")
+            {
+                int[,] area = new int[23, 10];
+                positionOnGrid = new int[23, 10];
+                timer = new Stopwatch();
+                timerC = new Stopwatch();
+                timerE = new Stopwatch();
+                speed = 300;
+                letFall = false;
+                keyPulse = false;
+                lineScore = 0;
+                points = 0;
+                level = 1;
+                GC.Collect();
+                Console.Clear();
+                Main();
+            }
+            else return;
+
+        }
+        private static void BlockMover() {
+            int combo = 0;
+            for (int i = 0; i < 23; i++)
+            {
+                int j;
+                for (j = 0; j < 10; j++)
+                {
+                    if (positionOnGrid[i, j] == 0)
+                        break;
+                }
+                if (j == 10)
+                {
+                    lineScore++;
+                    combo++;
+                    for (j = 0; j < 10; j++)
+                    {
+                        positionOnGrid[i, j] = 0;
+                    }
+                    int[,] newdropLocationGrid = new int[23, 10];
+                    for (int k = 1; k < i; k++)
+                    {
+                        for (int l = 0; l < 10; l++)
+                        {
+                            newdropLocationGrid[k + 1, l] = positionOnGrid[k, l];
+                        }
+                    }
+                    for (int k = 1; k < i; k++)
+                    {
+                        for (int l = 0; l < 10; l++)
+                        {
+                            positionOnGrid[k, l] = 0;
+                        }
+                    }
+                    for (int k = 0; k < 23; k++)
+                        for (int l = 0; l < 10; l++)
+                            if (newdropLocationGrid[k, l] == 1)
+                                positionOnGrid[k, l] = 1;
+                    Draw();
+                }
+            }
+            if (combo == 1)
+                points += 40 * level;
+            else if (combo == 2)
+                points += 100 * level;
+            else if (combo == 3)
+                points += 300 * level;
+            else if (combo > 3)
+                points += 300 * combo * level;
+
+            if (lineScore < 5) level = 1;
+            else if (lineScore < 10) level = 2;
+            else if (lineScore < 15) level = 3;
+            else if (lineScore < 25) level = 4;
+            else if (lineScore < 35) level = 5;
+            else if (lineScore < 50) level = 6;
+            else if (lineScore < 70) level = 7;
+            else if (lineScore < 90) level = 8;
+            else if (lineScore < 110) level = 9;
+            else if (lineScore < 150) level = 10;
+
+
+            if (combo > 0)
+            {
+                Console.SetCursorPosition(25, 0);
+                Console.WriteLine("Level: " + level);
+                Console.SetCursorPosition(25, 1);
+                Console.WriteLine("Points: " + points);
+                Console.SetCursorPosition(25, 2);
+                Console.WriteLine("Score: " + lineScore);
+            }
+
+            speed = 300 - 22 * level;
+        }
+        private static void Pulse() {
+            
+            if (Console.KeyAvailable)
+            {
+                key = Console.ReadKey();
+                keyPulse = true;
+            }
+            else
+                keyPulse = false;
+
+            if (Program.key.Key == ConsoleKey.LeftArrow & !tetrimino.IsSomethLeft() & keyPulse)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    tetrimino.position[i][1] -= 1;
+                }
+                Update();
+
+            }
+            else if (Program.key.Key == ConsoleKey.RightArrow & !tetrimino.IsSomethRight() & keyPulse)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    tetrimino.position[i][1] += 1;
+                }
+                Update();
+            }
+            if (Program.key.Key == ConsoleKey.DownArrow & keyPulse)
+            {
+                tetrimino.Solve();
+            }
+            if (Program.key.Key == ConsoleKey.DownArrow & keyPulse)
+            {
+                for (; tetrimino.IsSomthBelow() != true;)
+                {
+                    tetrimino.Solve();
+                }
+            }
+            if (Program.key.Key == ConsoleKey.UpArrow & keyPulse)
+            {
+                tetrimino.Rotate();
+                tetrimino.Update();
+            }
+        }
+    
+        public static void Draw() {
+            for (int i = 0; i < 23; ++i)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.SetCursorPosition(1 + 2 * j, i);
+                    if (area[i, j] == 1 | positionOnGrid[i, j] == 1)
+                    {
+                        Console.SetCursorPosition(1 + 2 * j, i);
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(block);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+
+            }
+        }
+        public static void GameArea()
+        {            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int lengthCount = 0; lengthCount <= 22; lengthCount++)
+            {
+                Console.SetCursorPosition(0, lengthCount);
+                Console.Write("†");
+                Console.SetCursorPosition(21, lengthCount);
+                Console.Write("†");
+            }
+            Console.SetCursorPosition(0, 23);
+            for (int widthCount = 0; widthCount <= 10; widthCount++)
+            {
+                Console.Write("--");
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        public static void Update() 
+        {
+            while (true)
+            {
+                time = (int)timerC.ElapsedMilliseconds;
+                if (time > speed)
+                {
+                    time = 0;
+                    timerC.Restart();
+                    tetrimino.Solve();
+                }
+                if (letFall == true)
+                {
+                    tetrimino = nextTetrimino;
+                    nextTetrimino = new Tetrimino();
+                    tetrimino.Appeareance();
+
+                    letFall = false;
+                }
+                int j;
+                for (j = 0; j < 10; j++)
+                {
+                    if (positionOnGrid[0, j] == 1)
+                        return;
+                }
+
+                Pulse();
+                BlockMover();
+            }
+        }
+        //public static int Rotate(int posX, int posY, int rot)
+        //{
+        //    switch (rot % 4)
+        //    {
+        //        case 0:
+        //            return posY * 4 + posX;
+        //        case 1:
+        //            return 12 + posY - (posX * 4);
+        //        case 2:
+        //            return 15 - (posY * 4) - posX;
+        //        case 3:
+        //            return 3 - posY + (posX * 4);
+        //        default:
+        //            break;
+        //    }
+        //    return 0;
+        //}
+        //private static void Appeareance()
+        //{            
+        //    Tetrimino tetrimino = new Tetrimino();
+        //    for (int i = 0; i < tetrimino.figure.GetLength(0); i++)
+        //    {
+        //        for (int j = 0; j < tetrimino.figure.GetLength(1); j++)
+        //        {
+        //            if (tetrimino.figure[i, j] == 1)
+        //            {
+        //                tetrimino.position.Add(new int[] { i, (10 - tetrimino.figure.GetLength(1)) / 2 + j });
+        //            }
+        //        }
+        //    }
+        //    Update();
+        //}
+        public void Soltar() { }
+    }
+}
+
---
+++ Tetris/Tetrimino.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Tetris
+{
+    class Tetrimino
+    {
+
+        public static int[,] I = new int[1, 4] { { 1, 1, 1, 1 } };
+        public static int[,] O = new int[2, 2] { { 1, 1 }, { 1, 1 } };
+        public static int[,] T = new int[2, 3] { { 0, 1, 0 }, { 1, 1, 1 } };
+        public static int[,] S = new int[2, 3] { { 0, 1, 1 }, { 1, 1, 0 } };
+        public static int[,] Z = new int[2, 3] { { 1, 1, 0 }, { 0, 1, 1 } };
+        public static int[,] J = new int[2, 3] { { 1, 0, 0 }, { 1, 1, 1 } };
+        public static int[,] L = new int[2, 3] { { 0, 0, 1 }, { 1, 1, 1 } };
+        public static List<int[,]> tetrimino = new List<int[,]>() { I, O, T, S, Z, J, L };
+
+        private bool isRight = false;
+        public int[,] figure;
+        public List<int[]> position = new List<int[]>();
+
+        public Tetrimino()
+        {
+
+            Random random = new Random();
+            figure = tetrimino[random.Next(0, 7)];
+            for (int i = 23; i < 33; ++i)
+            {
+                for (int j = 3; j < 10; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write("  ");
+                }
+
+            }
+
+            Program.GameArea();
+
+            for (int i = 0; i < figure.GetLength(0); i++)
+            {
+                for (int j = 0; j < figure.GetLength(1); j++)
+                {
+                    if (figure[i, j] == 1)
+                    {
+                        Console.SetCursorPosition(((10 - figure.GetLength(1)) / 2 + j) * 2 + 20, i + 5);
+                        Console.Write(Program.block);
+                    }
+                }
+            }
+        }
+        public void Appeareance()
+        {
+            for (int i = 0; i < figure.GetLength(0); i++)
+            {
+                for (int j = 0; j < figure.GetLength(1); j++)
+                {
+                    if (figure[i, j] == 1)
+                    {
+                        position.Add(new int[] { i, (10 - figure.GetLength(1)) / 2 + j });
+                    }
+                }
+            }
+            Update();
+        }
+
+        public void Solve()
+        {
+            if (IsSomthBelow())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Program.positionOnGrid[position[i][0], position[i][1]] = 1;
+                }
+                Program.letFall = true;
+
+            }
+            else
+            {
+                for (int numCount = 0; numCount < 4; numCount++)
+                {
+                    position[numCount][0] += 1;
+                }
+                Update();
+            }
+        }
+        public void Rotate()
+        {
+            List<int[]> posicionTemporal = new List<int[]>();
+            for (int i = 0; i < figure.GetLength(0); i++)
+            {
+                for (int j = 0; j < figure.GetLength(1); j++)
+                {
+                    if (figure[i, j] == 1)
+                        posicionTemporal.Add(new int[] { i, (10 - figure.GetLength(1)) / 2 + j });
+
+                }
+            }
+
+            if (figure == tetrimino[0])
+            {
+                if (isRight == false)
+                {
+                    for (int i = 0; i < position.Count; i++)
+                    {
+                        posicionTemporal[i] = TransformMatrix(position[i], position[2], "Clockwise");
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < position.Count; i++)
+                    {
+                        posicionTemporal[i] = TransformMatrix(position[i], position[2], "Counterclockwise");
+                    }
+                }
+            }
+
+            else if (figure == tetrimino[3])
+            {
+                for (int i = 0; i < position.Count; i++)
+                {
+                    posicionTemporal[i] = TransformMatrix(position[i], position[3], "Clockwise");
+                }
+            }
+
+            else if (figure == tetrimino[1]) return;
+            else
+            {
+                for (int i = 0; i < position.Count; i++)
+                {
+                    posicionTemporal[i] = TransformMatrix(position[i], position[2], "Clockwise");
+                }
+            }
+
+
+            for (int count = 0; OverlapLeft(posicionTemporal) != false | OverlapRight(posicionTemporal) != false | Overlap(posicionTemporal) != false; count++)
+            {
+                if (OverlapLeft(posicionTemporal) == true)
+                {
+                    for (int i = 0; i < position.Count; i++)
+                    {
+                        posicionTemporal[i][1] += 1;
+                    }
+                }
+
+                if (OverlapRight(posicionTemporal) == true)
+                {
+                    for (int i = 0; i < position.Count; i++)
+                    {
+                        posicionTemporal[i][1] -= 1;
+                    }
+                }
+                if (Overlap(posicionTemporal) == true)
+                {
+                    for (int i = 0; i < position.Count; i++)
+                    {
+                        posicionTemporal[i][0] -= 1;
+                    }
+                }
+                if (count == 3)
+                {
+                    return;
+                }
+            }
+
+            position = posicionTemporal;
+
+        }
+        public bool IsSomthBelow()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (position[i][0] + 1 >= 23)
+                    return true;
+                if (position[i][0] + 1 < 23)
+                {
+                    if (Program.positionOnGrid[position[i][0] + 1, position[i][1]] == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool IsSomethRight()
+        {
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (position[i][1] == 9)
+                    {
+                        return true;
+                    }
+                    else if (Program.positionOnGrid[position[i][0], position[i][1] + 1] == 1)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        public bool IsSomethLeft()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (position[i][1] == 0)
+                {
+                    return true;
+                }
+                else if (Program.positionOnGrid[position[i][0], position[i][1] - 1] == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int[] TransformMatrix(int[] coord, int[] axis, string dir)
+        {
+            int[] pcoord = { coord[0] - axis[0], coord[1] - axis[1] };
+            if (dir == "Counterclockwise")
+            {
+                pcoord = new int[] { -pcoord[1], pcoord[0] };
+            }
+            else if (dir == "Clockwise")
+            {
+                pcoord = new int[] { pcoord[1], -pcoord[0] };
+            }
+
+            return new int[] { pcoord[0] + axis[0], pcoord[1] + axis[1] };
+        }
+
+        public bool? OverlapLeft(List<int[]> location)
+        {
+            List<int> coordinates = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                coordinates.Add(location[i][1]);
+                if (location[i][1] < 0)
+                {
+                    return true;
+                }
+                if (location[i][1] > 9)
+                {
+                    return false;
+                }
+                if (location[i][0] >= 23)
+                    return null;
+                if (location[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (coordinates.Max() - coordinates.Min() == 3)
+                {
+                    if (coordinates.Min() == location[i][1] | coordinates.Min() + 1 == location[i][1])
+                    {
+                        if (Program.positionOnGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (coordinates.Min() == location[i][1])
+                    {
+                        if (Program.positionOnGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public bool? OverlapRight(List<int[]> location)
+        {
+            List<int> coordinates = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                coordinates.Add(location[i][1]);
+                if (location[i][1] > 9)
+                {
+                    return true;
+                }
+                if (location[i][1] < 0)
+                {
+                    return false;
+                }
+                if (location[i][0] >= 23)
+                    return null;
+                if (location[i][0] < 0)
+                    return null;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (coordinates.Max() - coordinates.Min() == 3)
+                {
+                    if (coordinates.Max() == location[i][1] | coordinates.Max() - 1 == location[i][1])
+                    {
+                        if (Program.positionOnGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (coordinates.Max() == location[i][1])
+                    {
+                        if (Program.positionOnGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public bool? Overlap(List<int[]> location)
+        {
+            List<int> coordinates = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                coordinates.Add(location[i][0]);
+                if (location[i][0] >= 23)
+                    return true;
+                if (location[i][0] < 0)
+                    return null;
+                if (location[i][1] < 0)
+                    return null;
+                if (location[i][1] > 9)
+                    return null;
+
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (coordinates.Max() - coordinates.Min() == 3)
+                {
+                    if (coordinates.Max() == location[i][0] | coordinates.Max() - 1 == location[i][0])
+                    {
+                        if (Program.positionOnGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (coordinates.Max() == location[i][0])
+                    {
+                        if (Program.positionOnGrid[location[i][0], location[i][1]] == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < 23; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Program.area[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Program.area[position[i][0], position[i][1]] = 1;
+            }
+            Program.Draw();
+
+        }
+    }
+}
